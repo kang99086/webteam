68f06521a3bf8e9088b760ff180fd922cb3300f9
park4608 <dusgml4608@naver.com>
2019-11-03 17:42:29 +0900

    test file

--- selab.hanyang.ac.kr/login/script/login.js
+++
@@ -1,8 +0,0 @@
-$( document ).ready(function(){
-	$("#idform").focus();
-	$("#error").slideDown("slow");
-	var timer = setInterval(function () {
-		$("#error").slideUp("slow");
-		clearInterval(timer);
-	}, 2000);
-});
