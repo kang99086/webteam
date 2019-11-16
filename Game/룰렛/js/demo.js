eac6c447daafefa98254e235bf1ea6c8d0c54466
YoungMinYoo <dudals0406@naver.com>
2019-11-16 18:43:42 +0900

    game

---
+++ Game/룰렛/js/demo.js
@@ -0,0 +1,53 @@
+(function() {
+  'use strict';
+
+  /*
+  $('input').on('keyup', function(e) {
+    console.log($(this).val());
+  });
+  */
+
+  var settings = [
+    {
+      el: 'wheel-1',
+      members: ['두더지잡기', '벽돌깨기', '스네이크게임', '카드짝맞추기'],
+      colors: ['#C7181D', '#FCB937', '#A1B836', '#371979'],
+      radius: 250,
+      startAngle: 'random'
+    }
+  ];
+
+  var wheels = [
+    new Wheel(settings[0])
+  ];
+
+  wheels.forEach(function(wheel) {
+    wheel.init();
+  });
+
+  var spinBtn = document.querySelector('.btn-spin');
+  spinBtn.addEventListener('click', function(e) {
+    wheels.forEach(function(wheel) {
+      wheel.spin(function(member) {
+        alert(member);
+        if(member == ('두더지잡기')){
+          window.open("../두더지잡기/index.html");
+          self.close();
+        }
+        else if( member == ('벽돌깨기')){
+          window.open("../벽돌깨기/break.html");
+          self.close();
+        }
+        else if(member == ('스네이크게임')){
+          window.open("../스네이크/index.html");
+          self.close();
+        }
+        else{
+          window.open("../짝맞추기/index.html");
+          self.close();
+        }
+      });
+  }, false);
+  });
+
+})();
