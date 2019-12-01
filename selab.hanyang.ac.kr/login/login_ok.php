f5362b7e744f7e75c827afbf709f59f9434b8c0a
park4608 <dusgml4608@naver.com>
2019-12-01 16:56:34 +0900

    add new login file

---
+++ selab.hanyang.ac.kr/login/login_ok.php
@@ -0,0 +1,29 @@
+<?php
+	include "../DB_Connect.php";
+	include "../top_login.php";
+	$db = connect();
+	$id=$_POST['id'];
+	$pw=$_POST['password'];
+
+	$login_sql = "select id,pw from students where id='$id'";
+	$login_stt=$db->prepare($login_sql);
+	$login_stt->execute();
+
+	foreach($login_stt as $lo){
+			if(password_verify($pw,$lo['pw']) and $lo['id']==$id){
+				$_SESSION['id']=$_POST['id'];
+				echo
+				"<script>
+					window.alert('로그인 되었습니다.');
+					location.href='../index.html';
+				</script>";
+			}
+			else{
+				echo
+				"<script>
+					window.alert('아이디 또는 비밀번호가 일치하지 않습니다.');
+					history.back(-1);
+				</script>";
+			}
+	}
+?>
