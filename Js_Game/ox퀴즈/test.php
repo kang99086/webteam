12a4adcffefefd1484c2e2a0d5d0bb40590c9dfa
YoungMinYoo <dudals0406@naver.com>
2019-12-15 16:29:15 +0900

    ox퀴즈

---
+++ Js_Game/ox퀴즈/test.php
@@ -0,0 +1,11 @@
+<html>
+<head>
+<body>
+<p>1번문제 : <?php echo $_POST["answer1"] ?></p>
+<p>2번문제 : <?php echo $_POST["answer2"] ?></p>
+<p>3번문제 : <?php echo $_POST["answer3"] ?></p>
+<p>4번문제 : <?php echo $_POST["answer4"] ?></p>
+<p>5번문제 : <?php echo $_POST["answer5"] ?></p>
+</body>
+</head>
+</html>
