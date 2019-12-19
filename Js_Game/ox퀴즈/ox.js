b6143c7db03e1d03fd8101a60e5e08158af5fd6a
park4608 <dusgml4608@naver.com>
2019-12-19 22:03:06 +0900

    update ox

--- Js_Game/ox퀴즈/ox.js
+++ Js_Game/ox퀴즈/ox.js
@@ -1,27 +1,45 @@
 var check = 1;
 
-function clicks(){
-  if(check == 1){
-    document.getElementById("quiz_1").style.display="none";
-    document.getElementById("quiz_2").style.visibility="visible";
+window.onload = function() {
+  const allIconImg = document.querySelectorAll("img");
+
+  allIconImg.forEach(img => img.onclick = clicks);
+}
+
+function clicks() {
+  const quizNum = document.querySelector("#quizbox fieldset h1");
+  const quiz1 = document.getElementById("quiz1"),
+    quiz2 = document.getElementById("quiz2"),
+    quiz3 = document.getElementById("quiz3"),
+    quiz4 = document.getElementById("quiz4"),
+    quiz5 = document.getElementById("quiz5");
+  if (check == 1) {
+    quizNum.innerText = "문제 " + (check+1) + "번";
+    quiz1.classList.add("display_none");
+    quiz2.classList.add("display_block");
     check = 2;
-  }else if(check == 2){
-    document.getElementById("quiz_2").style.display="none";
-    document.getElementById("quiz_3").style.visibility="visible";
+  } else if (check == 2) {
+    quizNum.innerText = "문제 " + (check+1) + "번";
+    quiz2.classList.remove("display_block");
+    quiz3.classList.add("display_block");
     check = 3;
-  }else if(check == 3){
-    document.getElementById("quiz_3").style.display="none";
-    document.getElementById("quiz_4").style.visibility="visible";
+  } else if (check == 3) {
+    quizNum.innerText = "문제 " + (check+1) + "번";
+    quiz3.classList.remove("display_block");
+    quiz4.classList.add("display_block");
     check = 4;
-  }else if(check == 4){
-    document.getElementById("quiz_4").style.display="none";
-    document.getElementById("quiz_5").style.visibility="visible";
+  } else if (check == 4) {
+    quizNum.innerText = "문제 " + (check+1) + "번";
+    quiz4.classList.remove("display_block");
+    quiz5.classList.add("display_block");
     check = 5;
-  }else if(check == 5){
-    document.getElementById("subm").style.visibility="visible";
-    check=6;
-  }else if(check==6){
-    document.getElementById("quizbox").style.display="none";
-    document.getElementById("resultbox").style.visibility="visible";
+  } else if (check == 5) {
+    quizNum.innerText = "Done!";
+    quiz5.classList.remove("display_block");
+    document.getElementById("subm").classList.add("display_block");
+    check = 6;
+  } else if (check == 6) {
+    document.getElementById("quizbox").classList.add("display_none");
+    document.getElementById("resultbox").style.visibility = "visible";
   }
 }
--- Js_Game/ox퀴즈/ox.php
+++ Js_Game/ox퀴즈/ox.php
@@ -1,82 +1,94 @@
 <html>
 <head>
-  <link rel="stylesheet" href="ox.css">
+  <link rel="stylesheet" href="oxUpgrade.css">
 </head>
 <body>
-  <script src="ox.js"></script>
+  <script src="oxUpgrade.js"></script>
   <div id="quizbox">
     <form method="post" id="ox_quiz" action="test.php">
-      <div id="quiz_1">
+      <div id="quizWrap">
         <fieldset>
-          <legend>문제 1번</legend>
-          문제1번 넣는곳<br/>
-          <div id="quiz_1O">
-            <p><label for="answer1_o"><img src="oo.png" width="40%" height="20%" alt="" onclick="clicks();"></label></p>
-            <p><input type="radio" name="answer1"  value="1" id="answer1_o"></p>
+          <legend> <h1>문제 1번</h1> </legend>
+          <p>문제 넣는곳</p>
+
+          <div id="quiz1">
+            <div class="ansO">
+                <label>
+                  <input type="radio" name="answer1"  value="1" class="O_icon">
+                  <img src="oo.png">
+                </label>
+            </div>
+            <div class="ansX">
+                <label>
+                  <input type="radio" name="answer1" value="0" class="X_icon">
+                  <img src="xx.png">
+                </label>
+            </div>
           </div>
-          <div id="quiz_1X">
-            <p><label for="answer1_x"><img src="xx.png" width="40%" height="20%" alt="" onclick="clicks();"></label></p>
-    				<p><input type="radio" name="answer1" value="0" id="answer1_x"></p>
+
+          <div id="quiz2" class="display_none">
+            <div class="ansO">
+                <label>
+                  <input type="radio" name="answer2"  value="1" class="O_icon">
+                  <img src="oo.png">
+                </label>
+            </div>
+            <div class="ansX">
+                <label>
+                  <input type="radio" name="answer2" value="0" class="X_icon">
+                  <img src="xx.png">
+                </label>
+            </div>
           </div>
+
+          <div id="quiz3" class="display_none">
+            <div class="ansO">
+                <label>
+                  <input type="radio" name="answer3"  value="1" class="O_icon">
+                  <img src="oo.png">
+                </label>
+            </div>
+            <div class="ansX">
+                <label>
+                  <input type="radio" name="answer3" value="0" class="X_icon">
+                  <img src="xx.png">
+                </label>
+            </div>
+          </div>
+
+          <div id="quiz4" class="display_none">
+            <div class="ansO">
+                <label>
+                  <input type="radio" name="answer4"  value="1" class="O_icon">
+                  <img src="oo.png">
+                </label>
+            </div>
+            <div class="ansX">
+                <label>
+                  <input type="radio" name="answer4" value="0" class="X_icon">
+                  <img src="xx.png">
+                </label>
+            </div>
+          </div>
+
+          <div id="quiz5" class="display_none">
+            <div class="ansO">
+                <label>
+                  <input type="radio" name="answer5"  value="1" class="O_icon">
+                  <img src="oo.png">
+                </label>
+            </div>
+            <div class="ansX">
+                <label>
+                  <input type="radio" name="answer5" value="0" class="X_icon">
+                  <img src="xx.png">
+                </label>
+            </div>
+          </div>
+          <div id="subm" class="display_none"><input type="submit" onclick="click();" value="제출하기"></div>
         </fieldset>
       </div>
-      <div id="quiz_2" style="visibility:hidden">
-        <fieldset>
-          <legend>문제 2번</legend>
-          문제2번 넣는곳<br/>
-          <div id="quiz_2O">
-            <p><label for="answer2_o"><img src="oo.png" width="40%" height="20%" alt="" onclick="clicks();"></label></p>
-            <p><input type="radio" name="answer2"  value=1 id="answer2_o"></p>
-          </div>
-          <div id="quiz_2X">
-            <p><label for="answer2_x"><img src="xx.png" width="40%" height="20%" alt="" onclick="clicks();"></label></p>
-    				<p><input type="radio" name="answer2" value=0 id="answer2_x"></p>
-          </div>
-        </fieldset>
-      </div>
-      <div id="quiz_3" style="visibility:hidden">
-        <fieldset>
-          <legend>문제 3번</legend>
-          문제3번 넣는곳<br/>
-          <div id="quiz_3O">
-            <p><label for="answer3_o"><img src="oo.png" width="40%" height="20%" alt="" onclick="clicks();"></label></p>
-            <p><input type="radio" name="answer3"  value=1 id="answer3_o"></p>
-          </div>
-          <div id="quiz_3X">
-            <p><label for="answer3_x"><img src="xx.png" width="40%" height="20%" alt="" onclick="clicks();"></label></p>
-    				<p><input type="radio" name="answer3" value=0 id="answer3_x"></p>
-          </div>
-        </fieldset>
-      </div>
-      <div id="quiz_4" style="visibility:hidden">
-        <fieldset>
-          <legend>문제 4번</legend>
-          문제4번 넣는곳<br/>
-          <div id="quiz_4O">
-            <p><label for="answer4_o"><img src="oo.png" width="40%" height="20%" alt="" onclick="clicks();"></label></p>
-            <p><input type="radio" name="answer4"  value=1 id="answer4_o"></p>
-          </div>
-          <div id="quiz_4X">
-            <p><label for="answer4_x"><img src="xx.png" width="40%" height="20%" alt="" onclick="clicks();"></label></p>
-    				<p><input type="radio" name="answer4" value=0 id="answer4_x"></p>
-          </div>
-        </fieldset>
-      </div>
-      <div id="quiz_5" style="visibility:hidden">
-        <fieldset>
-          <legend>문제 5번</legend>
-          문제5번 넣는곳<br/>
-          <div id="quiz_5O">
-            <p><label for="answer5_o"><img src="oo.png" width="40%" height="20%" alt="" onclick="clicks();"></label></p>
-            <p><input type="radio" name="answer5"  value=1 id="answer5_o"></p>
-          </div>
-          <div id="quiz_5X">
-            <p><label for="answer5_x"><img src="xx.png" width="40%" height="20%" alt="" onclick="clicks();"></label></p>
-    				<p><input type="radio" name="answer5" value=0 id="answer5_x"></p>
-          </div>
-        </fieldset>
-      </div>
-      <div id="subm" style="visibility:hidden"><input type="submit" onclick="click();" value="제출하기"></div>
+
     </form>
   </div>
 </body>
